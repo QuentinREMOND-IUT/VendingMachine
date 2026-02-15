f71d932eb7bfc9e3294b1f734bde98086c247c63
QuentinREMOND-IUT <quentin.remond@iut-dijon.u-bourgogne.fr>
2026-02-15 18:46:08 +0100

    Création de la base de la classe Product (Product.cs) avec propriétées et constructeur

---
+++ VendingMachine/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+
+        public Product(int id, string name, decimal price, int stock)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Price = price;
+            this.Stock = stock;
+        }
+    }
+}
