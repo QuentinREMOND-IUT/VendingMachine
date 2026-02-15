88a6927c84bacbe137b98797c2accc6a893f8cd5
QuentinREMOND-IUT <quentin.remond@iut-dijon.u-bourgogne.fr>
2026-02-15 18:54:59 +0100

    Création de la base de la classe VendingMachine (VendingMachine.cs) avec les propriétés ainsi que les méthodes à vide (simplement pour savoir quelles actions le distributeur pourra faire)

---
+++ VendingMachine/VendingMachine.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine
+{
+    public class VendingMachine
+    {
+        // La liste qui contient tous les objets "Produit"
+        public List<Product> Inventory { get; set; }
+        public decimal Balance { get; set; }
+
+        public VendingMachine()
+        {
+            Inventory = new List<Product>();
+            Balance = 0;
+        }
+
+        // Méthode pour ajouter de l'argent
+        public void InsertMoney(decimal amount)
+        {
+            // Plus tard
+        }
+
+        // Méthode pour acheter un produit
+        public string BuyProduct(int id)
+        {
+            // Plus tard
+            return "";
+        }
+
+        // Méthode pour rendre la monnaie
+        public decimal ReturnChange()
+        {
+            // Plus tard
+            return 0;
+        }
+    }
+}
